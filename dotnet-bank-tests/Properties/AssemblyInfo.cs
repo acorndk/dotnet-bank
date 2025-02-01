bf4baaadfdfb1e548a95e842bf76fd1d47404dd1
AcornDK <nastycoffee@hotmail.com>
2025-02-01 17:08:18 -0500

    Renamed unit test project

--- dotnet-bank-tests/Properties/AssemblyInfo.cs
+++ dotnet-bank-tests/Properties/AssemblyInfo.cs
@@ -2,18 +2,18 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("dotnet_bank_tests")]
+[assembly: AssemblyTitle("dotnet-bank-tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("dotnet_bank_tests")]
+[assembly: AssemblyProduct("dotnet-bank-tests")]
 [assembly: AssemblyCopyright("Copyright ©  2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 
-[assembly: Guid("4ba6af90-b7ab-4ead-ac52-5a5b51973660")]
+[assembly: Guid("b8a9156c-8c2d-4053-858a-3b6201530b23")]
 
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
---
+++ dotnet-bank-tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace dotnet_bank_tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
